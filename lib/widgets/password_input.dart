47b3b2b8399e8df84fd36ad415dedab309fbc5cf
Bay <bayurkp194@gmail.com>
2024-04-07 19:30:34 +0800

    feat: add custom password input field

--- lib/widgets/password_input.dart
+++ lib/widgets/password_input.dart
@@ -3,33 +3,49 @@ import 'package:progmob_1/constants/app_colors.dart';
 import 'package:progmob_1/constants/text_sizes.dart';
 
 class PasswordInput extends StatefulWidget {
-  const PasswordInput({super.key});
+  final TextEditingController controller;
+  final String hintText;
+
+  const PasswordInput({
+    super.key,
+    required this.controller,
+    required this.hintText,
+  });
 
   @override
   State<PasswordInput> createState() => _PasswordInputState();
 }
 
 class _PasswordInputState extends State<PasswordInput> {
+  bool _isHidden = true;
+
   @override
   Widget build(BuildContext context) {
     return TextField(
-      obscureText: true,
+      controller: widget.controller,
+      obscureText: _isHidden,
       obscuringCharacter: '•',
       textAlign: TextAlign.left,
       decoration: InputDecoration(
         contentPadding: const EdgeInsets.all(24.0),
-        hintText: 'Password',
+        hintText: widget.hintText,
         hintStyle: const TextStyle(
           fontSize: TextSizes.body,
           color: AppColors.neutral,
         ),
         fillColor: AppColors.base100,
         filled: true,
-        suffixIcon: Padding(
-          padding: const EdgeInsets.only(right: 16.0),
+        suffixIcon: Transform.translate(
+          offset: const Offset(-16, 0),
           child: IconButton(
-            icon: const Icon(Icons.remove_red_eye),
-            onPressed: () {},
+            icon: _isHidden
+                ? const Icon(Icons.visibility)
+                : const Icon(Icons.visibility_off),
+            onPressed: () {
+              setState(() {
+                _isHidden = !_isHidden;
+              });
+            },
           ),
         ),
         enabledBorder: OutlineInputBorder(
