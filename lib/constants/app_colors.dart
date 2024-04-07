f1055e7c9506b4780baf7ead7a89a61735489131
Bay <bayurkp194@gmail.com>
2024-04-07 13:05:54 +0800

    chore: add app colors constants

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+
+class AppColors {
+  static const base100 = Color(0xFFFFFFFF);
+  static const base200 = Color(0xFFF5F5F5);
+  static const base300 = Color(0xFFE0E0E0);
+  static const baseContent = Color(0xFF000000);
+
+  static const primary = Color(0xFF8B008B);
+  static const primaryContent = Color(0xFF000000);
+  static const secondary = Color(0xFFFFA500);
+  static const secondaryContent = Color(0xFF000000);
+  static const accent = Color(0xFF008080);
+  static const accentContent = Color(0xFF000000);
+  static const neutral = Color(0xFF808080);
+  static const neutralContent = Color(0xFFB3B3B3);
+}
