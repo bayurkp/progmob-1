bd5cb9ebf6dd2301e25f8a59485f4039ab3b1eda
Bay <bayurkp194@gmail.com>
2024-04-07 14:56:41 +0800

    chore: add black and white to app colors

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -1,13 +1,16 @@
 import 'dart:ui';
 
 class AppColors {
+  static const black = Color(0xFF000000);
+  static const white = Color(0xFFFFFFFF);
+
   static const base100 = Color(0xFFFFFFFF);
   static const base200 = Color(0xFFF5F5F5);
   static const base300 = Color(0xFFE0E0E0);
   static const baseContent = Color(0xFF000000);
 
   static const primary = Color(0xFF8B008B);
-  static const primaryContent = Color(0xFF000000);
+  static const primaryContent = Color(0xFFFFFFFF);
   static const secondary = Color(0xFFFFA500);
   static const secondaryContent = Color(0xFF000000);
   static const accent = Color(0xFF008080);
