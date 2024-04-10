10cbb55a83c14df8b34adaa0ba0ef974210ca717
Bay <bayurkp194@gmail.com>
2024-04-10 22:38:39 +0800

    feat: add named route

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:progmob_1/screens/home_screen.dart';
 import 'package:progmob_1/screens/login_screen.dart';
+import 'package:progmob_1/screens/register_screen.dart';
 
 void main() {
   runApp(const App());
@@ -11,11 +13,15 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      routes: {
+        '/': (context) => const HomeScreen(),
+        '/login': (context) => const LoginScreen(),
+        '/register': (context) => const RegisterScreen()
+      },
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: 'Poppins',
       ),
-      home: const LoginScreen(),
     );
   }
 }
